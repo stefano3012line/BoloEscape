69e18e780f7cc74a80003fd6b3b2911de1ef76eb
Stefano3012 <andreanesi1212@gmail.com>
2025-11-14 23:17:10 +0100

    riproduzione suono esempio

--- testdiroba.py
+++ testdiroba.py
@@ -6,9 +6,6 @@ import time
 game.init()
 mixer.init()
 Bolo_passing =mixer.Sound('bolognesi-passing.mp3')
-
-
-while True:
-    inpt = input('Press enter to play the sound: ')
-    Bolo_passing.play()  # Play the sound.
-    print('Playing sound')
+Bolo_passing.play()  # Play the sound.
+game.time.delay(4000)
+print('Playing sound')
