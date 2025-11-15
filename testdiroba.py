cc188d95af9bc477c760854b0ab00e582a734ae7
Francesco <tripodifrancesco03@gmail.com>
2025-11-15 01:16:17 +0100

    grafico velocià

--- testdiroba.py
+++ testdiroba.py
@@ -2,10 +2,18 @@ import pygame as game
 from pygame import mixer
 import numpy as np
 import time
-
+from matplotlib import pyplot as plt
 game.init()
 mixer.init()
 Bolo_passing =mixer.Sound('bolognesi-passing.mp3')
 Bolo_passing.play()  # Play the sound.
 game.time.delay(4000)
 print('Playing sound')
+
+y = np.array([600,720,800,861,911,954,992,1026,1053,1076,1097,1117,1135,1151,1166,1181,1195,1209,1222,1234,1246,1257,1268,1278,1288,1297,1306,1315,1324,1332,1340,1348,1356,1364,1371,1378,1385,1392,1399,1406,1413,1420,1426,1432,1438,1444,1450,1456,1462,1468,1474,1480,1485,1490,1495,1500,1505,1510,1515,1520,1525,1530,1535,1540,1545,1550])
+x = np.arange(len(y))
+
+plt.figure()
+plt.subplot(1,1,1)
+plt.plot(x,y)
+plt.show()
